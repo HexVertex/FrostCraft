23341bb6dbd765a446d98de9a0efc0372f832952
XEZKalvin <xezkalvin@gmail.com>
2014-02-23 20:22:48 +0100

    Added more biomes to the ThermalPump biome list

--- src/main/java/xelitez/frostcraft/registry/BiomeRegistry.java
+++ src/main/java/xelitez/frostcraft/registry/BiomeRegistry.java
@@ -23,28 +23,46 @@ public class BiomeRegistry
 	
 	static
 	{
-		addBiomeValue(BiomeGenBase.beach, 7);
-		addBiomeValue(BiomeGenBase.desert, 0);
-		addBiomeValue(BiomeGenBase.desertHills, 1);
-		addBiomeValue(BiomeGenBase.extremeHills, 22);
-		addBiomeValue(BiomeGenBase.extremeHillsEdge, 20);
-		addBiomeValue(BiomeGenBase.forest, 14);
-		addBiomeValue(BiomeGenBase.forestHills, 16);
-		addBiomeValue(BiomeGenBase.frozenOcean, 29);
-		addBiomeValue(BiomeGenBase.frozenRiver, 27);
-		addBiomeValue(BiomeGenBase.hell, 0);
-		addBiomeValue(BiomeGenBase.iceMountains, 28);
-		addBiomeValue(BiomeGenBase.icePlains, 25);
-		addBiomeValue(BiomeGenBase.jungle, 8);
-		addBiomeValue(BiomeGenBase.jungleHills, 9);
-		addBiomeValue(BiomeGenBase.mushroomIsland, 10);
-		addBiomeValue(BiomeGenBase.mushroomIslandShore, 11);
 		addBiomeValue(BiomeGenBase.ocean, 16);
 		addBiomeValue(BiomeGenBase.plains, 10);
+		addBiomeValue(BiomeGenBase.desert, 0);
+		addBiomeValue(BiomeGenBase.extremeHills, 22);
+		addBiomeValue(BiomeGenBase.forest, 14);
+		addBiomeValue(BiomeGenBase.taiga, 16);
+		addBiomeValue(BiomeGenBase.swampland, 11);
 		addBiomeValue(BiomeGenBase.river, 11);
+		addBiomeValue(BiomeGenBase.hell, 0);
 		addBiomeValue(BiomeGenBase.sky, 24);
-		addBiomeValue(BiomeGenBase.taiga, 28);
-		addBiomeValue(BiomeGenBase.taigaHills, 29);
+		addBiomeValue(BiomeGenBase.frozenOcean, 29);
+		addBiomeValue(BiomeGenBase.frozenRiver, 27);
+		addBiomeValue(BiomeGenBase.icePlains, 25);
+		addBiomeValue(BiomeGenBase.iceMountains, 30);
+		addBiomeValue(BiomeGenBase.mushroomIsland, 10);
+		addBiomeValue(BiomeGenBase.mushroomIslandShore, 11);	
+		addBiomeValue(BiomeGenBase.beach, 7);
+		addBiomeValue(BiomeGenBase.forestHills, 16);
+		addBiomeValue(BiomeGenBase.desertHills, 1);
+		addBiomeValue(BiomeGenBase.taigaHills, 17);
+		addBiomeValue(BiomeGenBase.extremeHillsEdge, 20);
+		addBiomeValue(BiomeGenBase.jungle, 8);
+		addBiomeValue(BiomeGenBase.jungleHills, 9);
+		addBiomeValue(BiomeGenBase.jungleEdge, 8);
+		addBiomeValue(BiomeGenBase.deepOcean, 16);
+		addBiomeValue(BiomeGenBase.stoneBeach, 9);
+		addBiomeValue(BiomeGenBase.coldBeach, 25);
+		addBiomeValue(BiomeGenBase.birchForest, 14);
+		addBiomeValue(BiomeGenBase.birchForestHills, 16);
+		addBiomeValue(BiomeGenBase.roofedForest, 18);
+		addBiomeValue(BiomeGenBase.coldTaiga, 28);
+		addBiomeValue(BiomeGenBase.coldTaigaHills, 29);
+		addBiomeValue(BiomeGenBase.megaTaiga, 20);
+		addBiomeValue(BiomeGenBase.megaTaigaHills, 22);
+		addBiomeValue(BiomeGenBase.extremeHillsPlus, 22);
+		addBiomeValue(BiomeGenBase.savanna, 7);
+		addBiomeValue(BiomeGenBase.savannaPlateau, 6);
+		addBiomeValue(BiomeGenBase.mesa, 4);
+		addBiomeValue(BiomeGenBase.mesaPlateau, 3);
+		addBiomeValue(BiomeGenBase.mesaPlateau_F, 3);
 	}
 	
 	public static boolean addBiomeValue(BiomeGenBase biome, int value)
