e7a7afbf9572d42b59c12b0358659d5f08e346a1
XEZKalvin <xezkalvin@gmail.com>
2014-03-14 15:01:19 +0100

    A bit more work on the Frost Guard

--- src/main/java/xelitez/frostcraft/client/render/RenderFrostGuard.java
+++ src/main/java/xelitez/frostcraft/client/render/RenderFrostGuard.java
@@ -1,22 +1,42 @@
 package xelitez.frostcraft.client.render;
 
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
+import xelitez.frostcraft.entity.EntityFrostGuard;
 
-public class RenderFrostGuard extends RenderLiving{
-
+public class RenderFrostGuard extends RenderBiped
+{
+	public ResourceLocation texture = new ResourceLocation("/texture.png");
+	
+	
 	public RenderFrostGuard(ModelBase par1ModelBase, float par2) 
 	{
-		super(par1ModelBase, par2);
-		// TODO Auto-generated constructor stub
+		super((ModelBiped) par1ModelBase, par2);
 	}
 
 	@Override
-	protected ResourceLocation getEntityTexture(Entity entity) {
-		// TODO Auto-generated method stub
-		return null;
+	protected ResourceLocation getEntityTexture(Entity entity) 
+	{
+		return texture;
 	}
 
+    protected void renderEquippedItems(EntityFrostGuard par1EntityFrostGuard, float par2)
+    {
+        super.renderEquippedItems((EntityLiving)par1EntityFrostGuard, par2);
+    }
+    
+    protected void renderEquippedItems(EntityLivingBase par1EntityLivingBase, float par2)
+    {
+        this.renderEquippedItems((EntityFrostGuard)par1EntityLivingBase, par2);
+    }
+    
+    protected void renderEquippedItems(EntityLiving par1EntityLiving, float par2)
+    {
+        this.renderEquippedItems((EntityFrostGuard)par1EntityLiving, par2);
+    }
 }
--- src/main/java/xelitez/frostcraft/entity/EntityFrostGuard.java
+++ src/main/java/xelitez/frostcraft/entity/EntityFrostGuard.java
@@ -1,18 +1,249 @@
 package xelitez.frostcraft.entity;
 
-import xelitez.frostcraft.registry.IdMap;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
+import xelitez.frostcraft.registry.IdMap;
 
 public class EntityFrostGuard extends EntityCreature
 {
-	
 	public EntityFrostGuard(World par1World) 
 	{
 		super(par1World);
-		this.setCurrentItemOrArmor(0, rand.nextInt(1) == 0 ? new ItemStack(IdMap.itemSpear) : new ItemStack(IdMap.itemCrossbow));
+        this.setSize(0.6F, 1.8F);
+        this.experienceValue = 5;
 	}
 	
+    protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(40.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.3D);
+        this.getAttributeMap().registerAttribute(SharedMonsterAttributes.attackDamage);
+        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(3.0D);
+    }
+	
+    protected boolean isAIEnabled()
+    {
+        return true;
+    }
+    
+    protected int getExperiencePoints(EntityPlayer par1EntityPlayer)
+    {
+        if (this.isChild())
+        {
+            this.experienceValue = (int)((float)this.experienceValue * 2.5F);
+        }
+
+        return super.getExperiencePoints(par1EntityPlayer);
+    }
+    
+    public IEntityLivingData onSpawnWithEgg(IEntityLivingData par1EntityLivingData)
+    {
+		this.setCurrentItemOrArmor(0, rand.nextFloat() < 0.5F ? new ItemStack(IdMap.itemSpear) : new ItemStack(IdMap.itemCrossbow));
+		this.setEquipmentDropChance(0, 0.125F);
+		this.setEquipmentDropChance(1, 0.025F);
+		this.setEquipmentDropChance(2, 0.025F);
+		this.setEquipmentDropChance(3, 0.025F);
+		this.setEquipmentDropChance(4, 0.025F);
+		this.addRandomArmor();
+        this.enchantEquipment();
+        return super.onSpawnWithEgg(par1EntityLivingData);
+    }
+    
+    protected void addRandomArmor()
+    {
+    	int i;
+        if (this.rand.nextFloat() < (this.worldObj.difficultySetting == EnumDifficulty.HARD ? 0.05F : 0.01F))
+        {
+        	for(i = 0;i < 4;i++)
+        	{
+        		this.setCurrentItemOrArmor(i + 1, new ItemStack(getArmorItemForSlot(i + 1, 4)));
+        	}
+        }
+        else
+        {
+        	for(i = 0;i < 4;i++)
+        	{
+        		this.setCurrentItemOrArmor(i + 1, new ItemStack(getArmorItemForSlot(i + 1, 1)));
+        	}
+        }
+    }
+    
+    public void onLivingUpdate()
+    {
+        this.updateArmSwingProgress();
+        float f = this.getBrightness(1.0F);
+
+        if (f > 0.5F)
+        {
+            this.entityAge += 2;
+        }
+
+        super.onLivingUpdate();
+    }
+
+    protected String getSwimSound()
+    {
+        return "game.hostile.swim";
+    }
+
+    protected String getSplashSound()
+    {
+        return "game.hostile.swim.splash";
+    }
+
+    /**
+     * Finds the closest player within 16 blocks to attack, or null if this Entity isn't interested in attacking
+     * (Animals, Spiders at day, peaceful PigZombies).
+     */
+    protected Entity findPlayerToAttack()
+    {
+        EntityPlayer entityplayer = this.worldObj.getClosestVulnerablePlayerToEntity(this, 16.0D);
+        return entityplayer != null && this.canEntityBeSeen(entityplayer) ? entityplayer : null;
+    }
+
+    /**
+     * Called when the entity is attacked.
+     */
+    public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
+    {
+        if (this.isEntityInvulnerable())
+        {
+            return false;
+        }
+        else if (super.attackEntityFrom(par1DamageSource, par2))
+        {
+            Entity entity = par1DamageSource.getEntity();
+
+            if (this.riddenByEntity != entity && this.ridingEntity != entity)
+            {
+                if (entity != this)
+                {
+                    this.entityToAttack = entity;
+                }
+
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the sound this mob makes when it is hurt.
+     */
+    protected String getHurtSound()
+    {
+        return "game.hostile.hurt";
+    }
+
+    /**
+     * Returns the sound this mob makes on death.
+     */
+    protected String getDeathSound()
+    {
+        return "game.hostile.die";
+    }
+
+    protected String func_146067_o(int p_146067_1_)
+    {
+        return p_146067_1_ > 4 ? "game.hostile.hurt.fall.big" : "game.hostile.hurt.fall.small";
+    }
+
+    public boolean attackEntityAsMob(Entity par1Entity)
+    {
+        float f = (float)this.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
+        int i = 0;
+
+        if (par1Entity instanceof EntityLivingBase)
+        {
+            f += EnchantmentHelper.getEnchantmentModifierLiving(this, (EntityLivingBase)par1Entity);
+            i += EnchantmentHelper.getKnockbackModifier(this, (EntityLivingBase)par1Entity);
+        }
+
+        boolean flag = par1Entity.attackEntityFrom(DamageSource.causeMobDamage(this), f);
+
+        if (flag)
+        {
+            if (i > 0)
+            {
+                par1Entity.addVelocity((double)(-MathHelper.sin(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F), 0.1D, (double)(MathHelper.cos(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F));
+                this.motionX *= 0.6D;
+                this.motionZ *= 0.6D;
+            }
+
+            int j = EnchantmentHelper.getFireAspectModifier(this);
+
+            if (j > 0)
+            {
+                par1Entity.setFire(j * 4);
+            }
+
+            if (par1Entity instanceof EntityLivingBase)
+            {
+                EnchantmentHelper.func_151384_a((EntityLivingBase)par1Entity, this);
+            }
+
+            EnchantmentHelper.func_151385_b(this, par1Entity);
+        }
+
+        return flag;
+    }
+
+    /**
+     * Basic mob attack. Default to touch of death in EntityCreature. Overridden by each mob to define their attack.
+     */
+    protected void attackEntity(Entity par1Entity, float par2)
+    {
+        if (this.attackTime <= 0 && par2 < 2.0F && par1Entity.boundingBox.maxY > this.boundingBox.minY && par1Entity.boundingBox.minY < this.boundingBox.maxY)
+        {
+            this.attackTime = 20;
+            this.attackEntityAsMob(par1Entity);
+        }
+    }
+
+
+    protected boolean isValidLightLevel()
+    {
+        int i = MathHelper.floor_double(this.posX);
+        int j = MathHelper.floor_double(this.boundingBox.minY);
+        int k = MathHelper.floor_double(this.posZ);
+
+        if (this.worldObj.getSavedLightValue(EnumSkyBlock.Sky, i, j, k) > this.rand.nextInt(32))
+        {
+            return false;
+        }
+        else
+        {
+            int l = this.worldObj.getBlockLightValue(i, j, k);
+
+            if (this.worldObj.isThundering())
+            {
+                int i1 = this.worldObj.skylightSubtracted;
+                this.worldObj.skylightSubtracted = 10;
+                l = this.worldObj.getBlockLightValue(i, j, k);
+                this.worldObj.skylightSubtracted = i1;
+            }
+
+            return l <= this.rand.nextInt(8);
+        }
+    }
 	
 }
